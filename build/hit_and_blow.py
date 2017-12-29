a20d1c1762135b05740801a3d4b5e4d8ecc7dbfe
dola963ke <dola963ke@gmail.com>
2017-12-30 00:22:59 +0900

    python2系からpython3系構文へ修正した。関数に変更した。

--- build/hit_and_blow.py
+++ build/hit_and_blow.py
@@ -1,15 +1,17 @@
 from random import shuffle
-N = 4
-a = range(1, 10)
-shuffle(a)
-a = ''.join(map(str,a[:N]))
-x = ''
-while x != a:
-    hit, blow = 0, 0
-    x = raw_input('Input {0} digit number: '.format(N))
-    for i, e in enumerate(a):
-        if x[i] == e:
-            hit += 1
-        elif e in x:
-            blow += 1
-    print '{0} hit, {1} blow'.format(hit, blow)
+
+def execute():
+    N = 4
+    a = list(range(1, 10))
+    shuffle(a)
+    a = ''.join(map(str,a[:N]))
+    x = ''
+    while x != a:
+        hit, blow = 0, 0
+        x = input('Input {0} digit number: '.format(N))
+        for i, e in enumerate(a):
+            if x[i] == e:
+                hit += 1
+            elif e in x:
+                blow += 1
+        print('{0} hit, {1} blow'.format(hit, blow))
